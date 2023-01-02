69c59a82d98f9587ead58582dae952aad832de53
SyimykRavshanbekov <programmist00707@gmail.com>
2023-01-02 10:56:15 +0600

    Initial commit

---
+++ src/main/java/com/example/argen_jetim/ArgenJetimApplication.java
@@ -0,0 +1,13 @@
+package com.example.argen_jetim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgenJetimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgenJetimApplication.class, args);
+    }
+
+}
